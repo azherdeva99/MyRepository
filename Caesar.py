58fa2a619ae7c4e4ffd8161dc97d0a7c7ff7858f
azherdeva99 <zherdeva@phys.vsu.ru>
2018-06-23 18:29:30 +0300

    Caesar's cipher

---
+++ Caesar.py
@@ -0,0 +1,16 @@
+abcEng='abcdefghijklmnopqrstuvwxyz'
+abcRu='абвгдеёжзийклмнопрстуфхцчшщъьыэюя'
+text=input('Input text: ').lower()
+k=int(input('Enter the shift number: '))
+new=''
+
+for c in text:
+    if c in abcEng:
+        new += abcEng[(abcEng.index(c) + k) % (len(abcEng))]
+for c in text:
+    if c in abcRu:
+        new += abcRu[(abcRu.index(c) + k) % (len(abcRu))]
+
+print('Ciphertext: ', new)
+
+
